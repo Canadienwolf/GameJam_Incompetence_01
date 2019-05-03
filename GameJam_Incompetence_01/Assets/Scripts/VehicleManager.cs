6f13a637318e3848620016cb9fe7388e5732a5ab
canadienwolf@gmail.com <canadienwolf@gmail.com>
2019-05-03 22:31:45 +0200

    a

---
+++ GameJam_Incompetence_01/Assets/Scripts/VehicleManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleManager : MonoBehaviour
+{
+
+    public GameObject[] cars;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
