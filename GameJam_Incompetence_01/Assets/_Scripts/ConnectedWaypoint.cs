72c9867995931d0788cef138f865f48a77119446
canadienwolf@gmail.com <canadienwolf@gmail.com>
2019-05-05 16:50:37 +0200

    asdf

---
+++ GameJam_Incompetence_01/Assets/_Scripts/ConnectedWaypoint.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+
+namespace Assets.Code
+{
+    public class ConnectedWaypoint : MonoBehaviour
+    {
+        [SerializeField]
+        protected float _connectivityRadius = 50f;
+
+        List<ConnectedWaypoint> _connections;
+        public float debugDrawRadius;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //Grab all waypoints in the scene
+            GameObject[] allWaypoints = GameObject.FindGameObjectsWithTag("Waypoints");
+
+            //create a list of waypoints i can refer to later.
+            _connections = new List<ConnectedWaypoint>();
+
+            //Check if they're a connected waypoint.
+            for (int i = 0; i < allWaypoints.Length; i++)
+            {
+                ConnectedWaypoint nextWaypoint = allWaypoints[i].GetComponent<ConnectedWaypoint>();
+
+                //i.e we found a waypoint.
+                if (nextWaypoint != null)
+                {
+                    if (Vector3.Distance(this.transform.position, nextWaypoint.transform.position) <= _connectivityRadius && nextWaypoint != this)
+                    {
+                        _connections.Add(nextWaypoint);
+                    }
+                }
+            }
+        }
+        
+        public void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, debugDrawRadius);
+
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position, _connectivityRadius);
+        }
+
+        public ConnectedWaypoint NextWaypoint(ConnectedWaypoint previousWaypoint)
+        {
+            if (_connections.Count == 0)
+            {
+                //No waypoints? Return null and complain
+                Debug.Log("Insufficient waypoint count");
+                return null;
+            }
+            else if (_connections.Count == 1 && _connections.Contains(previousWaypoint))
+            {
+                //Only one waypoint and it's the previous one? Just use that.
+                return previousWaypoint;
+            }
+            else //otherwise, find a random one that isn't the previous one.
+            {
+                ConnectedWaypoint nextWaypoint;
+                int nextIndex = 0;
+
+                do
+                {
+                    nextIndex = UnityEngine.Random.Range(0, _connections.Count);
+                    nextWaypoint = _connections[nextIndex];
+
+                } while (nextWaypoint == previousWaypoint);
+
+                return nextWaypoint;
+                
+
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ GameJam_Incompetence_01/Assets/_Scripts/NPCConnectedPatrol.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using System.Text;
+using System.Linq;
+using Assets.Code;
+
+namespace Asset.Code
+{
+    public class NPCConnectedPatrol : MonoBehaviour
+    {
+
+        //Dictates wheter the agent waits on each node.
+        [SerializeField]
+        bool _patrolWaiting;
+
+        //The total timw we wait at each node.
+        [SerializeField]
+        float _totaleWaitTime = 3f;
+
+        //The probability of switching direction.
+        [SerializeField]
+        float _switchProbability = 0.2f;
+
+        //Private variables for base behaviour.
+        NavMeshAgent _navMeshAgent;
+        ConnectedWaypoint _currentWaypoint;
+        ConnectedWaypoint _previousWaypoint;
+
+        bool _travelling;
+        bool _waiting;
+        float _waitTimer;
+        int _waypointsVisited;
+
+        // Start is called before the first frame update
+        public void Start()
+        {
+            _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+            if (_navMeshAgent == null)
+            {
+                Debug.LogError("The nav mesh agent component is not attached to " + gameObject.name);
+            }
+            else
+            {
+                if (_currentWaypoint == null)
+                {
+                    //Set it at random.
+                    //Grab all waypoints objects in scene.
+                    GameObject[] allWaypoints = GameObject.FindGameObjectsWithTag("Waypoints");
+
+                    if (allWaypoints.Length > 0)
+                    {
+                        while (_currentWaypoint == null)
+                        {
+                            int random = UnityEngine.Random.Range(0, allWaypoints.Length);
+                            ConnectedWaypoint startingWaypoint = allWaypoints[random].GetComponent<ConnectedWaypoint>();
+
+                            //i.e we found waypoint
+                            if (startingWaypoint != null)
+                            {
+                                _currentWaypoint = startingWaypoint;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Debug.Log("Failed to find any waypoints for use in the scene.");
+                    }
+                }
+
+                SetDestination();
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //check if we're close to the destination.
+            if (_travelling && _navMeshAgent.remainingDistance <= 1.0f)
+            {
+                _travelling = false;
+                _waypointsVisited++;
+
+                //If we're going to wait, then wait.
+                if (_patrolWaiting)
+                {
+                    _waiting = true;
+                    _waitTimer = 0f;
+                }
+                else
+                {
+                    SetDestination();
+                }
+            }
+
+            //Instead if we're waiting.
+            if (_waiting)
+            {
+                _waitTimer += Time.deltaTime;
+                if (_waitTimer >= _totaleWaitTime)
+                {
+                    _waiting = false;
+
+                    SetDestination();
+                }
+            }
+        }
+
+        private void SetDestination()
+        {
+            if (_waypointsVisited > 0)
+            {
+                ConnectedWaypoint nextWaypoint = _currentWaypoint.NextWaypoint(_previousWaypoint);
+                _previousWaypoint = _currentWaypoint;
+                _currentWaypoint = nextWaypoint;
+            }
+
+            Vector3 targetVector = _currentWaypoint.transform.position;
+            _navMeshAgent.SetDestination(targetVector);
+            _travelling = true;
+        }
+
+    }
+}
