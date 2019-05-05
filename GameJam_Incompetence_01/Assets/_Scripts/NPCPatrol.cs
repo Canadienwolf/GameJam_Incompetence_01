0a86aa9903f1af8f002eca0f359cd560066efac4
canadienwolf <canadienwolf@gmail.com>
2019-05-05 07:40:31 +0200

    Fiddling with AI, almost complete.

---
+++ GameJam_Incompetence_01/Assets/_Scripts/NPCPatrol.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCPatrol : MonoBehaviour
+{
+
+    //Dictates whether the NPC waits on each node.
+    [SerializeField]
+    bool _patrolWaiting;
+
+    //The total time we wait at each node.
+    [SerializeField]
+    float _totalWaitTime = 3f;
+
+    //The probability of switching direction.
+    [SerializeField]
+    float _switchProbability = 0.2f;
+
+    //List of all patrol nodes to visit.
+    [SerializeField]
+    //public GameObject [] _patrolPoints;
+    List<Waypoint> _patrolPoints;
+
+    //private variables for base behaviour.
+    NavMeshAgent _navMeshAgent;
+    int _currentPatrolIndex;
+    bool _travelling;
+    bool _waiting;
+    bool _patrolForward;
+    float _waitTimer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if (_navMeshAgent == null)
+        {
+            Debug.LogError("The nav mesh agent component is not attached to " + gameObject.name);
+        }
+        else
+        {
+            if (_patrolPoints != null && _patrolPoints.Count >= 2)
+            {
+                _currentPatrolIndex = 0;
+                SetDestination();
+            }
+            else
+            {
+                Debug.Log("Insufficient patrol points for basic patrolling behaviour.");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check if were close to the destination
+        if (_travelling && _navMeshAgent.remainingDistance <= 1.0f)
+        {
+            _travelling = false;
+
+            //if were going to wait, then wait
+            if (_patrolWaiting)
+            {
+                _waiting = true;
+                _waitTimer = 0f;
+            }
+            else
+            {
+                ChangePatrolPoint();
+                SetDestination();
+            }
+        }
+
+        //Instead if were waiting
+        if (_waiting)
+        {
+            _waitTimer += Time.deltaTime;
+            if (_waitTimer >= _totalWaitTime)
+            {
+                _waiting = false;
+
+                ChangePatrolPoint();
+                SetDestination();
+            }
+        }
+    }
+
+    private void SetDestination()
+    {
+        if (_patrolPoints != null)
+        {
+            Vector3 targetVector = _patrolPoints[_currentPatrolIndex].transform.position;
+            _navMeshAgent.SetDestination(targetVector);
+            _travelling = true;
+        }
+    }
+
+    /// <summary>
+    /// Selects a new patrol point in the available list, ubt
+    /// also with a small probability allows for us to move forward or backwards
+    /// </summary>
+    
+    private void ChangePatrolPoint()
+    {
+        if (UnityEngine.Random.Range(0f, 1f) <= _switchProbability)
+        {
+            _patrolForward = !_patrolForward;
+        }
+
+        if (_patrolForward)
+        {
+            _currentPatrolIndex = (_currentPatrolIndex + 1) % _patrolPoints.Count;
+        }
+        else
+        {
+            if (--_currentPatrolIndex < 0)
+            {
+                _currentPatrolIndex = _patrolPoints.Count - 1;
+            }
+        }
+    }
+}
